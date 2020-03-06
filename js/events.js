43d12c96a67e2f57b7c518a636098bebe7c4d9de
Роман Гилязов <gilyazov.roman2015@yandex.ru>
2020-03-06 07:32:53 +0500

    commit #2

---
+++ js/events.js
@@ -0,0 +1,28 @@
+﻿document.getElementById("click").onclick = function eventTuring() {
+    let count = getArrayOneSS(document.getElementById("in_count").value);
+
+
+    let str = '<table border="1"><tr>';
+    for (let i = 0; i < count.length * 2; i++) {
+        str += "<td " + 'id="' + i + '"' + '>' + '&nbsp;λ&nbsp;' + '</td>';
+    }
+    str += "</tr></table>";
+
+    let tree = document.querySelector(".table_good");
+    tree.innerHTML = str;
+
+    let str_settings = new Map([
+        ["updateMT", "Обновить МТ"],
+        ["step", "Сделать шаг"],
+        ["setRules", "Задать правила"],
+        ["myOptions", "Мой вариант Машины Тьюринга"]]);
+    str = getSettings(str_settings);
+    tree = document.querySelector('.settings');
+    tree.innerHTML = str;
+    // console.log( str ) ;
+
+    //let rulesHTML = document.querySelector('.rules');
+    //let rules = getRules();
+    //rulesHTML.innerHTML = rules;
+}
+
--- js/rules.js
+++ js/rules.js
@@ -1,4 +1,5 @@
-function getArrayOneSS($count){
+﻿
+function getArrayOneSS($count) {
     let count_oness = new Array();
     for(let i = 0; i <= $count; i++)
     {
@@ -6,11 +7,27 @@ function getArrayOneSS($count){
     }
     return count_oness;
 }
-function getRules(){
 
+function isset(variable)
+{
+    if(typeof(variable) != 'undefined' && variable != null)
+        return true;
+    else
+        return false;
 }
 
-document.getElementById("click").onclick = function eventTuring(){
-    let count = getArrayOneSS(document.getElementById("in_count").value);
+function getRules(){
     
 }
+
+function getSettings(parameters){
+    let str = '';
+    let key = parameters.keys();
+    for(let item of key)
+    {
+
+        str += '<button id="' + item + '">' + parameters.get(item) + '</button>\n';
+	}
+    return str;
+}
+
