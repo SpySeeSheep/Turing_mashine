55cf12208701a507153299b2ad4bf7713e4e5c78
Gilyazov <gilyazov.roman2015@yandex.ru>
2020-04-14 15:22:09 +0500

    seconds changes

--- js/events.js
+++ js/events.js
@@ -1,28 +1,103 @@
-﻿document.getElementById("click").onclick = function eventTuring() {
-    let count = getArrayOneSS(document.getElementById("in_count").value);
-
-
+﻿var count = 0;
+var configuration = new Array();
+var currentState = '11l';
+function update(){
+    currentState = '11l';
     let str = '<table border="1"><tr>';
-    for (let i = 0; i < count.length * 2; i++) {
-        str += "<td " + 'id="' + i + '"' + '>' + '&nbsp;λ&nbsp;' + '</td>';
+    let symbol = 'λ';
+    for (let i = 0; i < count.length * 4 + 1; i++) {
+        if(i >= Math.round((count.length * 2 + 1)*7/8) &&
+         i < Math.round((count.length * 2 + 1)*7/8) + count.length){
+            symbol='1';
+        }
+        else
+            symbol='λ';
+        str += "<td " + 'id="' + i + '"' + '>' + 
+        '<input type="text" class="cells" id="val'+i+'" value="'+symbol+'"></input>'
+         + '</td>';
     }
     str += "</tr></table>";
-
-    let tree = document.querySelector(".table_good");
-    tree.innerHTML = str;
-
-    let str_settings = new Map([
-        ["updateMT", "Обновить МТ"],
-        ["step", "Сделать шаг"],
-        ["setRules", "Задать правила"],
-        ["myOptions", "Мой вариант Машины Тьюринга"]]);
-    str = getSettings(str_settings);
-    tree = document.querySelector('.settings');
-    tree.innerHTML = str;
-    // console.log( str ) ;
-
-    //let rulesHTML = document.querySelector('.rules');
-    //let rules = getRules();
-    //rulesHTML.innerHTML = rules;
+    return str;
 }
 
+$('#click').click(function() {
+    const in_count = $('#in_count').val();
+    //  console.log(in_count);
+    count = getArrayOneSS(in_count);
+
+    $('.table_good').html(update());
+    let c = Math.round((count.length * 2 + 1)*7/8);
+    $('#val'+c).css('background-color', 'red');
+    currentPosition = c;
+    
+    $('.settings').css('visibility', 'visible');
+    $('.rules').html(getRules());
+});
+
+$('#updateMT').click(function() {
+    // console.log('updateMT');
+    $('.table_good').html(update());
+    let c = Math.round((count.length * 2 + 1)*7/8);
+    $('#val'+c).css('background-color', 'red');
+    currentPosition = c;
+});
+
+function step(symbol)
+{
+  console.log(currentState[2] == 'l');
+  $('#val'+currentPosition).val(rules[Number(currentState[1])][symbol][0]);
+  $('#val'+currentPosition).css('background-color','white');
+  currentState = rules[Number(currentState[1])][symbol];
+  currentPosition = (currentState[2] == 'l')?
+    currentPosition - 1: currentPosition + 1;
+  $('#val'+currentPosition).css('background-color', 'red');
+  return currentState;  
+}
+
+function next(currentState)
+{
+  
+  let symbol = 0;
+  switch($('#val'+currentPosition).val()){
+    case '1':
+        symbol = 0;
+        break;
+    case 'λ':
+        symbol = 1;
+        break;
+    case 'l':
+        symbol = 1;
+        break;
+    case '=':
+        symbol = 2;
+        break;
+    case '0':
+        symbol = 3;
+        break;
+  }
+  console.log(currentState);
+  if(rules[Number(currentState[1])][symbol][2] == 's') return 'end';
+  currentState = step(symbol);
+  return currentState;
+}
+
+function end()
+{
+  console.log('end');
+  $('#step').css('visibility', 'hidden');
+  $('#go').css('visibility', 'hidden');
+  let currPos = currentPosition;
+  while($('#val'+currPos).val() != 'l'){
+    $('#val'+currPos).css('background-color', 'rgb(245, 73, 99)');
+    currPos++;
+  }
+  alert('Итоговый результат с позиции: '+ currentPosition + 1);
+
+}
+
+$('#step').click(function(){
+    // console.log('step');
+    currentState = next(currentState);
+    if(currentState == 'end')
+        end();
+});
--- js/rules.js
+++ js/rules.js
@@ -1,4 +1,75 @@
-п»ї
+/*
+  ПРАВИЛА:
+  действий: 10
+  символов: 4
+
+  Описание: правил 10, из этого я использовал кострукцию из 3-х символов:
+  1 символ - символ который остается после выполнения действия
+  2 символ - новое состояние мт
+  3 символ - английская буква действия
+
+  спец. слово - empty служит обозначением пустого действия для текущего состояния. нужен
+  для того чтобы не путался порядок правил в массиве (для удобства работы).
+  порядок символов в правиле: 1 l(?) = 0
+  действия: r l s(завершающий символ)
+*/ 
+
+var currentPosition = 0;
+var rules = new Array();
+for(let i = 0; i < 10; ++i)
+{
+  rules.push(new Array());
+}
+rules[0].push('10l');
+rules[0].push('=1r');
+rules[0].push('=4l');
+rules[0].push('empty');
+
+rules[1].push('11r');
+rules[1].push('l2l');
+rules[1].push('empty');
+rules[1].push('empty');
+
+rules[2].push('l3l');
+rules[2].push('empty');
+rules[2].push('empty');
+rules[2].push('empty');
+
+rules[3].push('10l');
+rules[3].push('empty');
+rules[3].push('l7l');
+rules[3].push('empty');
+
+rules[4].push('06l');
+rules[4].push('05r');
+rules[4].push('empty');
+rules[4].push('15r');
+
+rules[5].push('15r');
+rules[5].push('empty');
+rules[5].push('=1r');
+rules[5].push('05r');
+
+rules[6].push('06l');
+rules[6].push('15r');
+rules[6].push('empty');
+rules[6].push('15r');
+
+rules[7].push('07l');
+rules[7].push('18l');
+rules[7].push('empty');
+rules[7].push('18l');
+
+rules[8].push('18l');
+rules[8].push('l9r');
+rules[8].push('empty');
+rules[8].push('08l');
+
+rules[9].push('19s');
+rules[9].push('empty');
+rules[9].push('empty');
+rules[9].push('empty');
+
 function getArrayOneSS($count) {
     let count_oness = new Array();
     for(let i = 0; i <= $count; i++)
@@ -8,26 +79,19 @@ function getArrayOneSS($count) {
     return count_oness;
 }
 
-function isset(variable)
-{
-    if(typeof(variable) != 'undefined' && variable != null)
-        return true;
-    else
-        return false;
-}
-
 function getRules(){
-    
+  let str = '';
+  
+  return str;
 }
 
 function getSettings(parameters){
     let str = '';
-    let key = parameters.keys();
+    const key = parameters.keys();
     for(let item of key)
     {
-
+        // console.log(item);
         str += '<button id="' + item + '">' + parameters.get(item) + '</button>\n';
 	}
     return str;
 }
-
